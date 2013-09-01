2156940143899bfe315305405627d4a959865045
zero <boy2000_007man@163.com>
2013-09-01 13:45:58 +0800

    version 0.1.0

--- KDE-RSMT/version.h
+++ KDE-RSMT/version.h
@@ -1,5 +1,5 @@
 #ifndef VERSION_H
 #define VERSION_H
 static const char name[] = "KDE-RMST";
-static const char version[] = "2013-09-01<p>v0.0.1";
+static const char version[] = "2013-09-01<p>v0.1";
 #endif // VERSION_H
